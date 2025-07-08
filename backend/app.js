6c1aa4e6c20e1c3495b852a1260ea68ee76b2ba4
TienNM <ojtgrowth@sendo.vn>
2025-07-08 11:12:55 +0700

    student feature

--- backend/app.js
+++ backend/app.js
@@ -48,6 +48,7 @@ var nurseRouter = require("./routes/nurse");
 var parentRouter = require("./routes/parent"); // Add the parent router
 var authRouter = require("./routes/auth"); // Add the auth router for authentication
 var adminRouter = require("./routes/admin"); // Add the admin router
+var studentRouter = require("./routes/student")
 
 var app = express();
 
@@ -80,6 +81,7 @@ app.use("/nurse", nurseRouter);
 app.use("/parent", parentRouter); // Use the parent router
 app.use("/auth", authRouter); // Add authentication routes
 app.use("/admin", adminRouter); // Use the admin router
+app.use("/student",studentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
